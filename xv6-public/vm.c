8fc156ad273a0b9f3b52dfca807745713da18f5b
OriQuack <kimdoh0423@naver.com>
2024-05-14 21:30:43 +0900

    a

--- xv6-public/vm.c
+++ xv6-public/vm.c
@@ -408,7 +408,7 @@ copyuvm(pde_t *pgdir, uint sz)
     if(!(*pte & PTE_P)){
       // panic("copyuvm: page not present");
     }
-    pa = PTE_ADDR(*pte);
+    // pa = PTE_ADDR(*pte);
     flags = PTE_FLAGS(*pte);
     if((mem = kalloc()) == 0)
       goto bad;
