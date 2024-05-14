aadfe510b5d5d8c8529dacf4fadffb146bd472ce
OriQuack <kimdoh0423@naver.com>
2024-05-15 00:48:05 +0900

    a

--- xv6-public/mytest.c
+++ xv6-public/mytest.c
@@ -35,6 +35,14 @@ int main(){
   if(pid == 0){
     printf(2, "CHILD: FREE MEM: %d\n", freemem());
     printf(2, "CHILD: -fd data: %c %c %c %c %c\n", src[0], src[1], src[2], src[3], src[8191]);
+    munmap(src);
+    printf(2, "CHILD: FREE MEM: %d\n", freemem());
+    char* new = (char*)mmap(0, 4096 * 3, PROT_READ, MAP_ANONYMOUS, -1, 0);
+    printf(2, "CHID: ANONY DONE\n");
+    printf(2, "CHILD: FREE MEM: %d\n", freemem());
+    printf(2, "CHID: ANONY access %c\n", new[0]);
+    printf(2, "CHILD: FREE MEM: %d\n", freemem());
+    
     exit();
   }
   else{
